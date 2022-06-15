2ac54a9bb22e40bc5de1c1e4d9e1db0601ea4bd7
Quiroga-ops <ccro60@misena.edu.co>
2022-06-15 16:12:42 -0500

    modulo crear clases

--- controladores/clases.controlador.php
+++ controladores/clases.controlador.php
@@ -1,1 +1,19 @@
 <?php
+/*=============================================
+MOSTRAR USUARIO
+=============================================*/
+class ControladorClases{
+
+	static public function ctrMostrarClases($item, $valor){
+        $tabla = "clases";
+
+		$respuesta = ModeloClases::MdlMostrarClases($tabla, $item, $valor);
+
+		return $respuesta;
+	}	
+
+
+}
+
+
+
--- vistas/modulos/crear-clases.php
+++ vistas/modulos/crear-clases.php
@@ -1,44 +1,345 @@
-<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Crear clases
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
-        <li class="active">Crear clases</li>
-      </ol>
-    </section>
 
-    <!-- Main content -->
-    <section class="content">
+  <section class="content-header">
+    
+    <h1>
+      
+      Administrar clases
+    
+    </h1>
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+    <ol class="breadcrumb">
+      
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Administrar clases</li>
+    
+    </ol>
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
-        </div>
-        <div class="box-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+  </section>
+
+  <section class="content">
+
+    <div class="box">
+
+      <div class="box-header with-border">
+  
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalCrearClase">
+          
+          Agregar clase
+
+        </button>
+
       </div>
-      <!-- /.box -->
 
-    </section>
-    <!-- /.content -->
+      <div class="box-body">
+        
+       <table class="table table-bordered table-striped dt-responsive tablas" width="100%">
+         
+        <thead>
+         
+         <tr>
+           
+           <th style="width:10px">#</th>
+           <th>Nombre clase</th>
+           <th>Duracion-horas</th>
+           <th>Horario</th>
+           <th>Docente a cargo</th>
+           <th>Estado</th>
+           <th>Acciones</th>
+
+         </tr> 
+
+        </thead>
+
+        <tbody>
+
+        <?php
+
+        $item = null;
+        $valor = null;
+
+        $clases = ControladorClases::ctrMostrarClases($item, $valor);
+
+        foreach ($clases as $key => $value){
+        
+          echo '<tr>
+                  <td>'.$value["id"].'</td>
+                  <td>'.$value["nombre_clase"].'</td>
+                  <td>'.$value["duracion"].'</td>
+                  <td>'.$value["horario"].'</td>
+                  <td>'.$value["docente"].'</td>
+                  <td>'.$value["estado"].'</td>
+
+                  
+                  
+
+                </tr>';
+        }
+
+
+        ?> 
+
+        </tbody>
+
+       </table>
+
+      </div>
+
+    </div>
+
+  </section>
+
+</div>
+
+<!--=====================================
+MODAL AGREGAR USUARIO
+======================================-->
+<div id="modalCrearClase" class="modal fade" role="dialog"><!--modal-->
+  <div class="modal-dialog"><!--modal-dialog-->
+    <div class="modal-content"><!--modal-content-->
+      <form role="form" method="post" enctype="multipart/form-data">
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Agregar clase</h4>
+        </div>
+
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+            <!-- ENTRADA PARA EL NOMBRE DE LA CLASE -->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+                <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Nombre de la clase" required>
+              </div>
+            </div>
+
+
+            <!-- ENTRADA PARA DURACION DE CLASE -->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-key"></i></span> 
+                <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder="Duracion" id="nuevoUsuario" required>
+              </div>
+            </div>
+
+
+            <!-- ENTRADA PARA EL HORARIO -->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span> 
+                <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Horario" required>
+              </div>
+            </div>
+
+
+            <!-- ENTRADA PARA NOMBRE DOCENTE -->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span> 
+                <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Docente" required>
+              </div>
+            </div>
+
+
+      
+
+
+
+
+
+        </div>
+</div>
+
+    <!--=====================================
+    PIE DEL MODAL
+    ======================================-->
+    <div class="modal-footer">
+      <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+      <button type="submit" class="btn btn-primary">Guardar clase</button>
+    </div>
+
+        <?php
+
+          $crearUsuario = new ControladorUsuarios();
+          $crearUsuario -> ctrCrearUsuario();
+
+        ?>
+
+      </form>
+    </div><!--modal-content-end-->
+  </div><!--modal-dialog-end--->
+</div><!--modal-end-->
+
+
+
+
+
+
+
+
+
+<!--=====================================
+MODAL EDITAR USUARIO
+======================================-->
+
+<div id="modalEditarUsuario" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" enctype="multipart/form-data">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Editar usuario</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" id="editarNombre" name="editarNombre" value="" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL USUARIO -->
+
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-key"></i></span> 
+
+                <input type="text" class="form-control input-lg" id="editarUsuario" name="editarUsuario" value="" readonly>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA LA CONTRASEÑA -->
+
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span> 
+
+                <input type="password" class="form-control input-lg" name="editarPassword" placeholder="Escriba la nueva contraseña">
+
+                <input type="hidden" id="passwordActual" name="passwordActual">
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA SELECCIONAR SU PERFIL -->
+
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-users"></i></span> 
+
+                <select class="form-control input-lg" name="editarPerfil">
+                  
+                  <option value="" id="editarPerfil"></option>
+
+                  <option value="Administrador">Administrador</option>
+
+                  <option value="Docente">Docente</option>
+
+                  <option value="Estudiante">Estudiante</option>
+
+                </select>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA SUBIR FOTO -->
+
+             <div class="form-group">
+              
+              <div class="panel">SUBIR FOTO</div>
+
+              <input type="file" class="nuevaFoto" name="editarFoto">
+
+              <p class="help-block">Peso máximo de la foto 2MB</p>
+
+              <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail previsualizar" width="100px">
+
+              <input type="hidden" name="fotoActual" id="fotoActual">
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Modificar usuario</button>
+
+        </div>
+
+     <?php
+
+          $editarUsuario = new ControladorUsuarios();
+          $editarUsuario -> ctrEditarUsuario();
+
+        ?> 
+
+      </form>
+
+    </div>
+
   </div>
-  <!-- /.content-wrapper -->
+
+</div>
+
+<?php
+
+  $borrarUsuario = new ControladorUsuarios();
+  $borrarUsuario -> ctrBorrarUsuario();
+
+?> 
+
+
